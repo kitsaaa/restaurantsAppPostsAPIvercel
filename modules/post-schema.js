c82a6a15d76222916b14928f88a47e238412d0fd
Nikita Korolkov <nikitakorolkov@Nikitas-MacBook-Pro.local>
2023-04-11 11:37:59 -0400

    added numberOfRatings

---
+++ modules/post-schema.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+let Schema = mongoose.Schema;
+
+let postSchema = new Schema({
+    title: String,
+    postDate: String,
+    featuredImage: String,
+    post: String,
+    postedBy: String,
+    rating: number,
+    numberOfRatings: number,
+    comments: [{author: String, comment: String, userID: String}],
+    isPrivate: Boolean,
+    views: String,
+    category: String
+}, {
+    versionKey: false
+});
+
+module.exports = postSchema;
